27cfb7836975394518c537abc223111acc137d56
pjammula <jpk2rock@gmail.com>
2025-02-06 02:42:43 +0530

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,9 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import swc from "@vitejs/plugin-swc";
+import swc from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    swc({
-      jsc: {
-        parser: {
-          syntax: "typescript",
-          tsx: true, // Enable TSX
-        },
-      },
-    }),
-  ],
+  plugins: [react(), swc()],
 });
