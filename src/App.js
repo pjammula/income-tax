7e89352c8b8bb970db85d112560ab8cfbc6eb637
pjammula <jpk2rock@gmail.com>
2025-02-06 03:04:16 +0530

    Remove Search and some config changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { Helmet } from "react-helmet";
 import Calculator from "./components/Calculator";
-import Search from "./components/Search";
+// import Search from "./components/Search";
 const App = () => {
-    return (_jsxs("div", { children: [_jsxs(Helmet, { children: [_jsx("title", { children: "Income Tax Calculator 2025 - New Tax Regime India" }), _jsx("meta", { name: "description", content: "Calculate your income tax in India under the new tax regime for 2025. Get accurate results for income tax slabs and deductions." }), _jsx("meta", { name: "keywords", content: "income tax, income tax 2025, new tax regime, india, india income tax 2025" }), _jsx("meta", { property: "og:title", content: "Income Tax Calculator 2025 - New Tax Regime India" }), _jsx("meta", { property: "og:description", content: "Calculate your income tax in India under the new tax regime for 2025." })] }), _jsx(Search, {}), _jsx(Calculator, {})] }));
+    return (_jsxs("div", { children: [_jsxs(Helmet, { children: [_jsx("title", { children: "Income Tax Calculator 2025 - New Tax Regime India" }), _jsx("meta", { name: "description", content: "Calculate your income tax in India under the new tax regime for 2025. Get accurate results for income tax slabs and deductions." }), _jsx("meta", { name: "keywords", content: "income tax, income tax 2025, new tax regime, india, india income tax 2025" }), _jsx("meta", { property: "og:title", content: "Income Tax Calculator 2025 - New Tax Regime India" }), _jsx("meta", { property: "og:description", content: "Calculate your income tax in India under the new tax regime for 2025." })] }), _jsx(Calculator, {})] }));
 };
 export default App;
